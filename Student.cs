5dac76485f427af56f2efbbea39dde3a1e989c33
lokesh1612lk <lokeshkumar1612lk@gmail.com>
2026-04-15 08:25:17 +0530

    Initial Project setup with console application and folder structure (models, Repository)

---
+++ Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace StudentManagementSystem.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Grade { get; set; }
+        public string Email { get; set; }
+
+        public Student(int Id, string Name, int Age, string Grade, string Email )
+        {
+            this.Id = Id;
+            this.Name = Name;
+            this.Age = Age;
+            this.Grade = Grade;
+            this.Email = Email; 
+        }
+
+        public override string ToString()
+        {
+            return $"| {Id,-4} | {Name,-20} | {Age,-4} | {Grade,-6} | {Email,-25} |";
+        }
+
+    }
+}

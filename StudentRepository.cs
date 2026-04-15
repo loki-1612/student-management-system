71cb3f8a1b29ca1095773921a1d11333b31481be
lokesh1612lk <lokeshkumar1612lk@gmail.com>
2026-04-15 09:07:39 +0530

    Implemented StudentRepository with CRUD operations for student management

---
+++ StudentRepository.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using StudentManagementSystem.Models;
+
+public class StudentRepository
+{
+    private List <Student> students = new List<Student> ();
+
+    public void AddStudent(Student student)
+    {
+        students.Add(student);
+    }
+
+    public List <Student> GetAllStudents()
+    {
+        return students;
+    }
+
+    public Student GetStudentById(int id)
+    {
+        foreach(var student in students)
+        {
+            if (student.Id == id)
+            {
+                return student;
+            }
+        }
+        return null;
+    }
+
+    public bool UpdateStudent(int id, string name, int age, string email, string grade)
+    {
+        var student = GetStudentById(id);
+        if (student != null)
+        {
+            student.Name = name;
+            student.Age = age;
+            student.Email = email;
+            student.Grade = grade;
+            return true;
+        }
+        return false;
+    }
+
+    public bool DeleteStudent(int id)
+    {
+        var student = GetStudentById(id);
+        if (student != null)
+        {
+            students.Remove(student);
+            return true;
+        }
+        return false;
+    }
+}
+
